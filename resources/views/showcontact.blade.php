8584adf5de8a49c6e16feec6749fac7a474b398f
duwikk <fmochammaddwi@gmail.com>
2022-10-28 12:43:50 +0700

    p

--- resources/views/showcontact.blade.php
+++ resources/views/showcontact.blade.php
@@ -17,8 +17,8 @@
 
             <div class="card-footer">
                    
-                <a href="{{ route('mastercontact.edit', $item->pivot->id)  }}" class="btn btn-sm btn-warning btn-circle"><i class="fas fa-edit"></i></a>
-                <a href="{{ route('mastercontact.hapus', $item->pivot->id)  }}" class="btn btn-sm btn-danger btn-circle"><i class="fas fa-trash"></i></a>
+                <a href="{{ route('mastercontact.edit', $item->id)  }}" class="btn btn-sm btn-warning btn-circle"><i class="fas fa-edit"></i></a>
+                <a href="{{ route('mastercontact.hapus', $item->id)  }}" class="btn btn-sm btn-danger btn-circle"><i class="fas fa-trash"></i></a>
                 
             </div>
         </div>
